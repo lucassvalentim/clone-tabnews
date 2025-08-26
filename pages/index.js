b3ac5fab45c707a7202aa12703ce63464551475f
Lucas Valentim <72881147+lucassvalentim@users.noreply.github.com>
2025-08-26 13:34:39 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home(){
+    return (
+    <div>
+    <h1>Se você é gay da uma risadinha.</h1>
+    <br></br>
+    <br></br>
+    <img src="https://i.pinimg.com/236x/ea/79/3d/ea793d058308ee86ed55be78b3ac78c4.jpg" />
+    </div>
+  );
+}
+
+export default Home;
